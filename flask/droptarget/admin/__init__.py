2f4343e8417dd39f6be33b456dff37c6cf99944d
Russell Dare <rdare3@gmail.com>
2020-05-04 22:30:26 -0700

    Took first pass at object display
    Using DataTables to display machines, locations, players
    
    Goal is to have some sort of object display where
    certain quick changes like Active, or Status can be
    set on the fly.  Additional details can be expanded in
    modal and edited in full there.  Also an add modal can
    be entered to add an entirely new record.

--- flask/droptarget/admin/__init__.py
+++ flask/droptarget/admin/__init__.py
@@ -16,19 +16,78 @@ playfield = PlayfieldAPI("host.docker.internal", "8080")
 @admin.route('/admin')
 @admin.route('/admin/home')
 def show_admin_home():
-    return render_template('admin-home.html', title="Admin - Home")
+    return render_template('admin-home.html',
+                           title="Admin - Home",
+                           obj_counts=_get_object_counts())
 
 
 @admin.route('/admin/machines')
 def show_admin_machines():
-    return render_template('admin-machines.html', title="Admin - Machines")
+    return render_template('admin-machines.html',
+                           title="Admin - Machines",
+                           obj_counts=_get_object_counts(),
+                           machine_data=_get_all_machines())
 
 
 @admin.route('/admin/locations')
 def show_admin_locations():
-    return render_template('admin-locations.html', title="Admin - Locations")
+    return render_template('admin-locations.html',
+                           title="Admin - Locations",
+                           obj_counts=_get_object_counts(),
+                           location_data=_get_all_locations())
 
 
 @admin.route('/admin/players')
 def show_admin_players():
-    return render_template('admin-players.html', title="Admin - Players")
+    return render_template('admin-players.html',
+                           title="Admin - Players",
+                           obj_counts=_get_object_counts(),
+                           player_data=_get_all_players())
+
+
+def _get_all_players():
+    player_json = playfield.api_request("get", "player", "all_players", None)
+
+    if player_json is not "Error" and player_json is not None:
+        player_entries = playfield.parse_json(player_json)
+    else:
+        # Fail silently
+        player_entries = "api_error"
+
+    return player_entries
+
+
+def _get_all_locations():
+    location_json = playfield.api_request("get", "location", "all_locations", None)
+
+    if location_json is not "Error" and location_json is not None:
+        location_entries = playfield.parse_json(location_json)
+    else:
+        # Fail silently
+        location_entries = "api_error"
+
+    return location_entries
+
+
+def _get_all_machines():
+    machine_json = playfield.api_request("get", "machine", "all_machines", None)
+
+    if machine_json is not "Error" and machine_json is not None:
+        machine_entries = playfield.parse_json(machine_json)
+    else:
+        # Fail silently
+        machine_entries = "api_error"
+
+    return machine_entries
+
+
+def _get_object_counts():
+    object_count_json = playfield.api_request("get", "general", "get_object_counts", None)
+
+    if object_count_json is not "Error" and object_count_json is not None:
+        obj_count_entries = playfield.parse_json(object_count_json)
+    else:
+        # Fail silently
+        obj_count_entries = "api_error"
+
+    return obj_count_entries
